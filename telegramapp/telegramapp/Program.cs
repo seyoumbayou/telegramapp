92862b23f0d405995d17e6d8ed17e6d291e4dac3
seyoumbayou <60073387+seyoumbayou@users.noreply.github.com>
2020-01-20 09:07:38 -0800

    initial commit

---
+++ telegramapp/telegramapp/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace telegramapp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+         var myaccount = new account();
+            myaccount.phonenumber = 120634;
+            myaccount.username = "abc";
+
+            var myaccount2 = new account();
+            myaccount2.phonenumber = 1234567;
+            myaccount2.username = "drtft";
+
+        }
+    }
+}
---
+++ telegramapp/telegramapp/account.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace telegramapp
+{
+    /// <summary>
+    /// this represents a telegram account
+    /// where you can chat or call
+    /// to your friends
+    /// </summary>
+    class account
+    {
+        #region properties
+        public int phonenumber { get; set; }
+        public string username { get; set; }
+        public string chat { get; set; }
+        public string groupchat { get; set; }
+        public string contact { get; set; }
+        public int call { get; set; }
+        #endregion 
+
+    }
+}
